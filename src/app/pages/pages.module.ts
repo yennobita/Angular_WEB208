a5839b95cb1ad70dfb3a7003d75f69328ce28325
Trần Danh Doanh <doanh.tran@bmbsoft.com.vn>
2023-02-20 17:45:37 +0700

    update sidebar

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,9 +4,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { PagesComponent } from './pages.component';
 import { PagesRoutingModule } from './pages-routing.module';
+import { HeaderComponent } from '../layout/header/header.component';
 
 @NgModule({
-  declarations: [PagesComponent],
+  declarations: [PagesComponent, HeaderComponent],
   imports: [
     CommonModule,
     PagesRoutingModule,
